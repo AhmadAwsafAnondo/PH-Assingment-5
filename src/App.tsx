98433b4ddb96d7999361c2002f18dcfa1620b5cf
Ahmad Awsaf Anondo <ahmadawsafanondo@gmail.com>
2026-09-13 21:17:56 +0600

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import TechnologyGrid from "./components/TechnologyGrid";
+import Footer from "./components/Footer";
 function App() {
 
   return (
     <>
-      
+      <div className="min-h-screen bg-white">
+      <Navbar />
+      <Hero />
+      <TechnologyGrid />
+      <Footer />
+      <ToastContainer position="bottom-right" autoClose={2500} />
+    </div>
     </>
   )
 }
